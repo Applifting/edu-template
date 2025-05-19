c3218d085b471b833c7779de2d7e416a117123eb
Petr <petr.vnenk@applifting.cz>
2025-05-19 12:14:47 +0200

    docs(be): add explanation comments to the http client service

--- backend/src/core/http-clients/http.client.ts
+++ backend/src/core/http-clients/http.client.ts
@@ -5,6 +5,15 @@ type GetParameters = Parameters<typeof axios.get>;
 type PostParameters = Parameters<typeof axios.post>;
 type PutParamenters = Parameters<typeof axios.put>;
 
+/**
+ * You can use this class to make HTTP requests to the backend.
+ * It is a wrapper around the axios library.
+ *
+ * Import the HttpModule in the @Module where you want to use this and then you can inject this HttpClient in your services.
+ *
+ * It's here only for your convenience, you can write your own/use whatever you'd prefer. It's generally a good idea to have wrappers for these things.
+ * so that you don't have to rewrite every usage across the codebase if you e.g. decide to replace axios with something else.
+ */
 @Injectable()
 export class HttpClient {
   readonly #instance = axios.create();
